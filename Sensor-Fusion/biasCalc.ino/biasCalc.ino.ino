67a01e6a4d59d6e57c7fff44537710020fb7f365
il-dionigi <brian.dionigi@gmail.com>
2017-11-16 20:37:17 -0800

    BiasCalc Helper + Test Take 2
    Helper for calcing bias and test for write/read reg

--- Sensor-Fusion/biasCalc.ino/biasCalc.ino.ino
+++ Sensor-Fusion/biasCalc.ino/biasCalc.ino.ino
@@ -1,4 +1,6 @@
-#include <sensor_fusion.cpp>
+#include <Arduino.h>
+#include <Wire.h>
+#include <sensor_fusion.h>
 
 #define ahx 59
 #define alx 60
@@ -14,69 +16,68 @@
 #define ghz 71
 #define glz 72
 
-double bax;
-double bay;
-double baz;
-double bgx;
-double bgy;
-double bgz;
 
+struct vector accelBias;
+struct vector gyroBias;
+
+void computeBiasComp(int lowBit, int highBit, float* dest) {
+  *dest = 0;
+  short ready;
+  short temp;
+
+    for (int i = 0; i < 75; i++) {
+    do
+      {
+        readRegister(58,&ready,1)
+      }while(!(ready & 0x01))
+
+
+    readRegister(highBit,&temp,1);
+    *dest += temp << 4;
+    readRegister(lowBit,&temp,1);
+    *dest += *dest + temp;
+  }
+  *dest = *dest / 75;
+}
 
 void setup(){
 
+  Wire.begin();
+  config();
+
+  uint8_t* pwrMgmt;
+  readRegister(107, pwrMgmt, 1);
+  Serial.print(*pwrMgmt, BIN);
+  Serial.print("\n");
+
+  uint8_t* set_full_scale;
+  readRegister(0x1B, set_full_scale, 1);
+  Serial.print(*set_full_scale, BIN);
+  Serial.print("\n");
+
+  uint8_t* con;
+  readRegister(26, con, 1);
+  Serial.print(*con, BIN);
+  Serial.print("\n");
   
-  unsigned long sumax = 0;
-  unsigned long sumay = 0;
-  unsigned long sumaz = 0;
+  computeBiasComp(alx, ahx, &accelBias.x);
+  Serial.print(accelBias.x);
+  Serial.print("\n");
+  computeBiasComp(aly, ahy, &accelBias.y);
+  Serial.print(accelBias.y);
+  Serial.print("\n");
+  computeBiasComp(alz, ahz, &accelBias.z);
+  Serial.print(accelBias.z);
+  Serial.print("\n");
 
-  unsigned long sumgx = 0;
-  unsigned long sumgy = 0;
-  unsigned long sumgz = 0;
+  computeBiasComp(glx, ghx, &gyroBias.x);
+  Serial.print(accelBias.x);
+  Serial.print("\n");
+  computeBiasComp(gly, ghy, &gyroBias.y);
+  Serial.print(accelBias.y);
+  Serial.print("\n");
+  computeBiasComp(glz, ghz, &gyroBias.z);
+  Serial.print(accelBias.z);
+  Serial.print("\n");
 
-  short ready;
-  short xh;
-  short xl;
-  short yh;
-  short yl;
-  short zh;
-  short zl;
-  
-  for(size_t i = 0; i < 75 ; i++)
-  {
-    do
-    {
-      readRegister(58,&ready,1)
-    }while(!(ready & 0x01))
-    
-    readRegister(ahx,&xh,1);
-    readRegister(alx,&xl,1);
-    readRegister(ahy,&yh,1);
-    readRegister(aly,&yl,1);
-    readRegister(ahz,&zh,1);
-    readRegister(alz,&zl,1);
-
-    sumax += (xh << 4 + xl);
-    sumay += (yh << 4 + yl);
-    sumaz += (zh << 4 + zl);
-
-    readRegister(ghx,&xh,1);
-    readRegister(glx,&xl,1);
-    readRegister(ghy,&yh,1);
-    readRegister(gly,&yl,1);
-    readRegister(ghz,&zh,1);
-    readRegister(glz,&zl,1);
-
-    sumgx += (xh << 4 + xl);
-    sumgy += (yh << 4 + yl);
-    sumgz += (zh << 4 + zl);
-  }
-  
-  bax = (sumax / 75.0);
-  bay = (sumay / 75.0);
-  baz = (sumaz / 75.0) - 2048.0;
-
-  bgx = sumgx /75.0;
-  bgy = sumgy / 75.0;
-  bgz = sumgz /75.0;
 }
-
