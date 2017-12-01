d75295160a01fae49d67b67627769d29cdc72977
il-dionigi <brian.dionigi@gmail.com>
2017-11-30 20:30:35 -0800

    Delay b/w Motor PWM
    Added 50ms delay between changing PWM of motor

--- Motor Control/motor_control/motor_control.ino
+++ Motor Control/motor_control/motor_control.ino
@@ -8,11 +8,13 @@ void loop() {
   for (i = 1; i <= 25; i++)
   {
     analogWrite(MOTOR, i);
+    delay(50);
   }
 
   for (i = 1; i <= 25; i++)
   {
     analogWrite(MOTOR, 25 - i);
+    delay(50);
   }
 
   delay(1000);
